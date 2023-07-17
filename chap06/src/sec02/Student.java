535b0ea96e5356a6297f86a72a4560a89324c2db
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-17 09:24:25 +0900

    Delete chap06/src directory

--- chap06/src/sec02/Student.java
+++
@@ -1,9 +0,0 @@
-package sec02;
-
-public class Student {
-	int age=30; // 나이
-	String name="홍길동"; // 이름
-	String dept="경영학과"; // 학과
-	String gender="남"; // 성별
-	
-}
--- chap06/src/sec06/exam02/mycompany/Car2.java
+++
@@ -1,9 +0,0 @@
-package sec06.exam02.mycompany;
-
-import sec06.exam02.hankook.*;
-
-public class Car2 {
-
-	Tire tr1 = new Tire(); // Tire public 접근 가능
-	//Tire2 tr2 = new Tire2(); // Tire2 default 접근 불가능 (only hankook package 내에서만 사용가능)
-}
