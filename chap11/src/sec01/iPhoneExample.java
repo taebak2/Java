a413e2227b30fdfd2a221995c5c991b04f443385
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-24 16:55:13 +0900

    Update iPhoneExample.java

--- chap11/src/sec01/iPhoneExample.java
+++ chap11/src/sec01/iPhoneExample.java
@@ -3,7 +3,7 @@ package sec01;
 public class iPhoneExample {
 
 	public static void main(String[] args) {
-		iPhone iphone = new iPhone("¾ÖÇÃ", 1500000, "iPhoneMax14");
+		iPhone iphone = new iPhone("ì• í”Œ", 1500000, "iPhoneMax14");
 		System.out.println(iphone.toString());
 
 	}
