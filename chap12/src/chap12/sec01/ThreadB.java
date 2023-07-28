5dfd39a8409530c44fd04dcfdf6cb2cddbad4864
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-28 09:23:44 +0900

    Update ThreadB.java

--- chap12/src/chap12/sec01/ThreadB.java
+++ chap12/src/chap12/sec01/ThreadB.java
@@ -4,7 +4,7 @@ public class ThreadB extends Thread{
 	@Override
 	public void run() {
 		for (int i = 0; i < 2; i++) {
-			System.out.println(getName() + "°¡ Ãâ·ÂÇÑ ³»¿ë");
+			System.out.println(getName() + "ê°€ ì¶œë ¥í•œ ë‚´ìš©");
 		}
 	}
 }
