a585edf7a439a4998309fa9213da1c45798e29d9
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-29 14:31:18 +0900

    Update BeepPrintExample4.java

--- chap12/src/chap12/sec01/BeepPrintExample4.java
+++ chap12/src/chap12/sec01/BeepPrintExample4.java
@@ -7,7 +7,7 @@ public class BeepPrintExample4 {
 	public static void main(String[] args) {
 
 		Thread thread = new BeepThread();
-		thread.start(); // thread.start run? vs Runnable run? 
+		thread.start();
 
 		for (int i = 0; i < 5; i++) {
 			System.out.println("띵");
