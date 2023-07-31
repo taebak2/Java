e771cae35bf724fdf9d34c0f2921507b0a089e50
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-07-31 11:36:37 +0900

    Update ATM.java

--- chap12/src/chap12/sec02/ATM.java
+++ chap12/src/chap12/sec02/ATM.java
@@ -9,10 +9,8 @@ public class ATM implements Runnable {
 			for (int i = 0; i < 10; i++) {
 				// object의 메소드
 
-				notify(); // mother.start()가 먼저 실행되면 for문의 i=0일때 notify는 실행되지 않는다. 잠든 사람이 없기 때문에 깨울 사람이 없는 상태임 
-				try {
-					 
-					wait(); // wait(); : 갖고 있던 고유 락을 해제하고 스레드를 잠들게 함 즉 mother가 잠이 들게된다.  
+				notify(); 	// mother.start()가 먼저 실행되면 for문의 i=0일때 notify는 실행되지 않는다. 잠든 사람이 없기 때문에 깨울 사람이 없는 상태임 
+				try {wait(); 	// wait(); : 갖고 있던 고유 락을 해제하고 스레드를 잠들게 함 즉 mother가 잠이 들게된다.  
 						//  son.start()가 실행될 때 son이 notify를 만나 잠든 mother가 일어나 mother.start();는 wait이후 명령문인 Thread.sleep(1000)부터 실행하게 된다.
 						// 그와 동시에 son은 wait를 만나서 자고있는 상태가 된다.
 						//  그리고 for문의 i=1일때 mother가 notify를 다시 만나기 때문에 그때 son이 일어난다.
