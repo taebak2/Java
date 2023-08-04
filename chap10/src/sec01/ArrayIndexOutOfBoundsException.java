5f6e5fe8595b4ba042ed3715163cfd035fc8b551
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-08-05 08:09:05 +0900

    Update ArrayIndexOutOfBoundsException.java

--- chap10/src/sec01/ArrayIndexOutOfBoundsException.java
+++ chap10/src/sec01/ArrayIndexOutOfBoundsException.java
@@ -11,7 +11,7 @@ public class ArrayIndexOutOfBoundsException {
 		System.out.println("a[0]:" + data1);
 
 		String data2 = args[0]; // args라는 배열을 생성하지 않고 배열에 있는 값을 대입함
-		String data3 = args[1]; // 이대로 출력하면 ArrayIndexException 발생(배열에서 인덱스 범위 초과한 오류)
+		String data3 = args[1]; // 출력하면 ArrayIndexException 발생(배열에서 인덱스 범위 초과한 오류)
 		String data4 = args[2];
 		System.out.println(data2);
 		System.out.println(data3);
