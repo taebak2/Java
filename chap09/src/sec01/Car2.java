094ecd8d699daa76125dff86505080efd4952052
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-08-04 16:34:44 +0900

    Update Car2.java

--- chap09/src/sec01/Car2.java
+++ chap09/src/sec01/Car2.java
@@ -4,7 +4,7 @@ public class Car2 {
 
 	public static void main(String[] args) {
 		Car car = new Car();
-		Car.OldCar oldcar = car.new OldCar(); // Car클래스 외부접근 -> A.B b = a.new B();
+		Car.OldCar oldcar = car.new OldCar(); // A.B b = a.new B();
 		oldcar.speedUp(50);
 		System.out.println(oldcar.speed);
 	}
