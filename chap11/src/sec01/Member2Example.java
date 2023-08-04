c3e4396c3e0a3370cd712c0f62ac833c0901872b
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-08-05 08:14:45 +0900

    Update Member2Example.java

--- chap11/src/sec01/Member2Example.java
+++ chap11/src/sec01/Member2Example.java
@@ -1,11 +1,8 @@
 package sec01;
 
 public class Member2Example {
-
 	public static void main(String[] args) {
 		Member2 member = new Member2("blue", "이파란");
 		System.out.println(member);
-
 	}
-
 }
