49db6560452f6585e3ad8cbeaddb778d94d00e60
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-08-11 16:34:37 +0900

    Add files via upload

--- chap14/src/sec02/DataInputOutputStreamExample.java
+++ chap14/src/sec02/DataInputOutputStreamExample.java
@@ -9,8 +9,8 @@ public class DataInputOutputStreamExample {
 
 	public static void main(String[] args) throws Exception {
 
-		FileOutputStream fos = new FileOutputStream("c:/temp/primitive.db");
-		DataOutputStream dos = new DataOutputStream(fos);
+		FileOutputStream fos = new FileOutputStream("c:/temp/primitive.db"); // 파일에 데이터를 출력하기 위한 스트림
+		DataOutputStream dos = new DataOutputStream(fos); // 기본 데이터 타입(primitive data types)을 이진 형태로 파일에 출력하는 스트림
 
 		dos.writeUTF("홍길동");
 		dos.writeDouble(95.5);
@@ -23,7 +23,7 @@ public class DataInputOutputStreamExample {
 		dos.flush();
 		dos.close();
 
-		FileInputStream fis = new FileInputStream("c:/temp/primitive.db");
+		FileInputStream fis = new FileInputStream("c:/temp/primitive.db"); // 입력 스트림 : 데이터를 읽어오는데 사용되는 스트림
 		DataInputStream dis = new DataInputStream(fis);
 
 		for (int i = 0; i < 2; i++) {
