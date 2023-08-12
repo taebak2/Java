e7398483b96dd495c3d986511add752a319c3f41
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-08-12 14:45:12 +0900

    Update WriteExmaple4.java

--- chap14/src/sec01/WriteExmaple4.java
+++ chap14/src/sec01/WriteExmaple4.java
@@ -7,11 +7,9 @@ public class WriteExmaple4 {
 
 	public static void main(String[] args) throws Exception{
 		/*
-		 * OutputStream os = new FileOutputStream("c:/temp/test2.db");
-		 * 
-		 * byte[] array = {10,20,30};
-		 * 
-		 * os.write(array); os.flush(); os.close();
+		  OutputStream os = new FileOutputStream("c:/temp/test2.db");
+		  byte[] array = {10,20,30};
+		  os.write(array); os.flush(); os.close();
 		 */
 		
 		
