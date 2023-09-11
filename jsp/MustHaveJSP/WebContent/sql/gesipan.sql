8fc3395d815108aac0fbd4826c6a66e4b79c330f
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-09-11 16:56:50 +0900

    Add files via upload

---
+++ jsp/MustHaveJSP/WebContent/sql/gesipan.sql
@@ -0,0 +1,15 @@
+select * from member;
+
+insert into board(title, content, id, visitcount)
+	values ('제목2입니다','내용2입니다','java', 3);
+	
+desc board;
+
+select * from member;
+
+show tables;
+
+select * from board;
+
+
+select count(*) from board;
