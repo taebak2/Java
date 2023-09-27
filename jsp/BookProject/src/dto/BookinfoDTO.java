a9ee553bc8f5ce4c4f43b15dda1e388d95e3f274
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-09-27 12:25:35 +0900

    Create BookinfoDTO.java

---
+++ jsp/BookProject/src/dto/BookinfoDTO.java
@@ -0,0 +1,67 @@
+package DTO;
+
+public class BookinfoDTO {
+	
+	private String bookid;
+	
+	private String author;
+	
+	private String booktitle;
+	
+	private String bookcontent;
+	
+	private int bookprice;
+	
+	private String bookdate;
+	
+	private String memberid;
+	
+	
+	
+	public String getBookid() {
+		return bookid;
+	}
+	public void setBookid(String bookid) {
+		this.bookid = bookid;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getBooktitle() {
+		return booktitle;
+	}
+	public void setBooktitle(String booktitle) {
+		this.booktitle = booktitle;
+	}
+	public String getBookcontent() {
+		return bookcontent;
+	}
+	public void setBookcontent(String bookcontent) {
+		this.bookcontent = bookcontent;
+	}
+	public int getBookprice() {
+		return bookprice;
+	}
+	public void setBookprice(int bookprice) {
+		this.bookprice = bookprice;
+	}
+	public String getBookdate() {
+		return bookdate;
+	}
+	public void setBookdate(String bookdate) {
+		this.bookdate = bookdate;
+	}
+	public String getMemberid() {
+		return memberid;
+	}
+	public void setMemberid(String memberid) {
+		this.memberid = memberid;
+	}
+
+	
+	
+	
+}
