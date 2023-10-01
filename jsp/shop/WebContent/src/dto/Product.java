7df237e0601f719b8b452bb2fc3af68b290f78b5
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-10-02 00:12:55 +0900

    Update Product.java

--- jsp/shop/WebContent/src/dto/Product.java
+++ jsp/shop/WebContent/src/dto/Product.java
@@ -6,14 +6,14 @@ public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private String productId; // 상품 아이디
-	private String pname; // 상품명
-	private Integer unitPrice; // 상품가격
-	private String description; // 상품설명
+	private String productId;    // 상품 아이디
+	private String pname; 	     // 상품명
+	private Integer unitPrice;   // 상품가격
+	private String description;  // 상품설명
 	private String manufacturer; // 제조회사
-	private String category; // 상품분류
-	private long unitsInStock; // 상품 재고 개수
-	private String condition; // 상품 상태 (신상,중고,재생)
+	private String category;     // 상품분류
+	private long unitsInStock;   // 상품 재고 개수
+	private String condition;    // 상품 상태 (신상,중고,재생)
 	
 	public Product(String productId) {
 		super(); //  부모 클래스의 기본 생성자(default constructor)를 호출
