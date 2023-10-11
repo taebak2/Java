f58f8f5ae792deb139b759219549e108d54cb83a
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-10-11 16:00:54 +0900

    Add files via upload

--- jsp/shop/src/dto/BoardDTO.java
+++ jsp/shop/src/dto/BoardDTO.java
@@ -8,65 +8,57 @@ public class BoardDTO {
 	private String postdate;
 	private int visitcount;
 	private String name;
+	private int virNum;
 	
-
+	public int getVirNum() {
+		return virNum;
+	}
+	public void setVirNum(int virNum) {
+		this.virNum = virNum;
+	}
 	public int getNum() {
 		return num;
 	}
-
 	public void setNum(int num) {
 		this.num = num;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getContent() {
 		return content;
 	}
-
 	public void setContent(String content) {
 		this.content = content;
 	}
-
 	public String getId() {
 		return id;
 	}
-
 	public void setId(String id) {
 		this.id = id;
 	}
-
 	public String getPostdate() {
 		return postdate;
 	}
-
 	public void setPostdate(String postdate) {
 		this.postdate = postdate;
 	}
-
 	public int getVisitcount() {
 		return visitcount;
 	}
-
 	public void setVisitcount(int visitcount) {
 		this.visitcount = visitcount;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
-	
+
+		
 	
 }
