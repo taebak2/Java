58e1412873a18f2545d6c09bd2ccc322a5fc0e91
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-10-13 14:43:41 +0900

    Add files via upload

--- jsp/MustHaveJSP/src/fileupload/MyfileDAO.java
+++ jsp/MustHaveJSP/src/fileupload/MyfileDAO.java
@@ -1,5 +1,8 @@
 package fileupload;
 
+import java.util.List;
+import java.util.Vector;
+
 import common.JDBConnect;
 
 public class MyfileDAO extends JDBConnect{
@@ -12,6 +15,7 @@ public class MyfileDAO extends JDBConnect{
 		int applyResult=0;
 		
 		try {
+			
 			String query = "insert into myfile values(seq_board_num.nextval, ?,?,?,?,?,default) ";
 			psmt = con.prepareStatement(query);
 			psmt.setString(1, dto.getName());
@@ -20,11 +24,44 @@ public class MyfileDAO extends JDBConnect{
 			psmt.setString(4, dto.getOfile());
 			psmt.setString(5, dto.getSfile());
 			applyResult = psmt.executeUpdate();
+			
 		} catch (Exception e) {
+			
 			e.printStackTrace();
 			System.out.println("파일 insert 중 오류 발생");
 		}
 		
 		return applyResult;
+	}
+	
+	public List<MyfileDTO> myFileList()	{
+		List<MyfileDTO> fileList = new Vector<>();	// Vector<>() : 여러 사람이 동시 사용가능?? 무슨말~?	
+		
+		String query = "select * from myfile order by idx desc";
+		
+		try {
+			psmt = con.prepareStatement(query);
+			rs = psmt.executeQuery();
+			
+			while(rs.next()) {
+				MyfileDTO dto = new MyfileDTO();
+				dto.setIdx(rs.getString(1));
+				dto.setName(rs.getString(2));
+				dto.setTitle(rs.getString(3));
+				dto.setCate(rs.getString(4));
+				dto.setOfile(rs.getString(5));
+				dto.setSfile(rs.getString(6));
+				dto.setPostdate(rs.getString(7));
+				fileList.add(dto);
+				
+			}
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			System.out.println("목록 가져오기 실패");
+		}
+		
+		return fileList;
 	}
 }
