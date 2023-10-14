09c2f5f3b4a44e2148089e95ca594f053e20b4c2
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-10-14 16:31:08 +0900

    Update MyfileDTO.java

--- jsp/MustHaveJSP/src/fileupload/MyfileDTO.java
+++ jsp/MustHaveJSP/src/fileupload/MyfileDTO.java
@@ -2,7 +2,7 @@ package fileupload;
 
 public class MyfileDTO {
 	
-	private String idx;		// 파일번호
+	private String idx;	// 파일번호
 	private String name;	// 작성자 이름
 	private String title;	// 제목
 	private String cate; 	// 카테고리
