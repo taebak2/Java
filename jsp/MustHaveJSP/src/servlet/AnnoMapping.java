31ca28fe4fc578ba3e00eaf8c55507ad2ad49cd3
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-10-16 17:38:12 +0900

    Update AnnoMapping.java

--- jsp/MustHaveJSP/src/servlet/AnnoMapping.java
+++ jsp/MustHaveJSP/src/servlet/AnnoMapping.java
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/13Servlet/AnnoMapping.do")	// web.xml이 아닌 Annotation으로 매핑하는 방식 
+@WebServlet("/13Servlet/AnnoMapping.do")	// Annotation으로 매핑하는 방식 
 public class AnnoMapping extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	// jsp의 method가 post, get이냐에 따라서 메서드 설정 
 		request.setAttribute("message", "웹서블릿으로 매핑");
 		request.getRequestDispatcher("/13Servlet/AnnoMapping.jsp").forward(request, response);
 	}
