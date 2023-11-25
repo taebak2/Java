751c97881c69c606eb17ae471742e83be15d375f
Kim-taegyun <xorbs3902@gmail.com>
2023-11-25 20:16:20 +0900

    Update VectorExample.java

--- review13/src/review/VectorExample.java
+++ review13/src/review/VectorExample.java
@@ -2,7 +2,7 @@ package review;
 
 import java.util.Vector;
 
-public class VectorExample {
+public class VectorExample { 
 
 	public static void main(String[] args) {
 		Vector<Point> v = new Vector<>();
